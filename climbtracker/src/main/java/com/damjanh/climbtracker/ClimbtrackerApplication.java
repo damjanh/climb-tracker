df1b5a634da85febf2d8e812d0da3d401aef1b83
damjanh <damjan.hudobreznik@gmail.com>
2023-03-27 12:28:41 +0200

    Project setup

---
+++ climbtracker/src/main/java/com/damjanh/climbtracker/ClimbtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.damjanh.climbtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbtrackerApplication.class, args);
+	}
+
+}
---
+++ climbtracker/src/test/java/com/damjanh/climbtracker/ClimbtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damjanh.climbtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
