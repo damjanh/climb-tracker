a01595cfd268d81c900d4ff6f124ae69152e1bc4
damjanh <damjan.hudobreznik@gmail.com>
2023-03-29 14:05:00 +0200

    Fix double loading and class name error.

--- climb-tracker-app/src/components/RouteItem.jsx
+++ climb-tracker-app/src/components/RouteItem.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const RouteItem = (props) => {
   const { title, description, grade } = props.route;
   return (
-    <div class="card m-3">
-      <div class="card-body">
-        <h5 class="card-title">{title}</h5>
-        <p class="card-text">{description}</p>
-        <p class="card-text">{grade}</p>
-        <a href="#" class="btn btn-primary">Redpoint</a>
+    <div className="card m-3">
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <p className="card-text">{grade}</p>
+        <a href="#" className="btn btn-primary">Redpoint</a>
       </div>
     </div>
   )
